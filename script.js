c0ad0440e1cd8c6f3db40d03dc61556f7ad95dcd
FamosaNegra <114776724+FamosaNegra@users.noreply.github.com>
2023-04-27 21:35:21 -0300

    Terminei o CSS

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    console.log("funcionou");
+
+    $('.carousel').carousel({
+        interval: 3000
+    })
+});
--- server.js
+++ server.js
@@ -8,41 +8,70 @@ const server = http.createServer((req, res) => {
     if (req.url === '/' || req.url === '/index.html') {
         fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
             if (err) {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.writeHead(500, {
+                    'Content-Type': 'text/plain'
+                });
                 res.end('Internal Server Error');
             } else {
-                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
                 res.end(data);
             }
         });
 
-    // Arquivo CSS
+        // Arquivo CSS
     } else if (req.url === '/style') {
         fs.readFile(path.join(__dirname, 'style.css'), (err, data) => {
             if (err) {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.writeHead(500, {
+                    'Content-Type': 'text/plain'
+                });
                 res.end('Internal Server Error');
             } else {
-                res.writeHead(200, {'Content-Type': 'text/css'});
+                res.writeHead(200, {
+                    'Content-Type': 'text/css'
+                });
                 res.end(data);
             }
         });
 
-    // Arquivo JavaScript
+        // Arquivo JavaScript
     } else if (req.url === '/script') {
         fs.readFile(path.join(__dirname, 'script.js'), (err, data) => {
             if (err) {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.writeHead(500, {
+                    'Content-Type': 'text/plain'
+                });
                 res.end('Internal Server Error');
             } else {
-                res.writeHead(200, {'Content-Type': 'text/javascript'});
+                res.writeHead(200, {
+                    'Content-Type': 'text/javascript'
+                });
                 res.end(data);
             }
         });
 
-    // Rota desconhecida
+        // Rota desconhecida
+    } else if (req.url.startsWith('/img/')) {
+        const imgPath = path.join(__dirname, req.url);
+        fs.readFile(imgPath, (err, data) => {
+            if (err) {
+                res.writeHead(500, {
+                    'Content-Type': 'text/plain'
+                });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, {
+                    'Content-Type': 'image/png'
+                });
+                res.end(data);
+            }
+        });
     } else {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.writeHead(404, {
+            'Content-Type': 'text/plain'
+        });
         res.end('Page Not Found');
     }
 });
